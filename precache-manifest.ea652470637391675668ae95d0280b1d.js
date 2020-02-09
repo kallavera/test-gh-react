a01ac36742a7c7d42df21551398557d9413ca3f7
kallavera <arield.velasco@gmail.com>
2020-02-09 13:51:37 -0300

    Updates

---
+++ precache-manifest.ea652470637391675668ae95d0280b1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0429f92aa67bfa1a1ffc436c9079bc4f",
+    "url": "/kallavera/test-gh-react/index.html"
+  },
+  {
+    "revision": "ecea7e83775914caee7c",
+    "url": "/kallavera/test-gh-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "80fca8941e5f1d76abb6",
+    "url": "/kallavera/test-gh-react/static/js/2.a5066339.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/kallavera/test-gh-react/static/js/2.a5066339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecea7e83775914caee7c",
+    "url": "/kallavera/test-gh-react/static/js/main.e8e3ab9a.chunk.js"
+  },
+  {
+    "revision": "524bcdfb43e04f2e4d62",
+    "url": "/kallavera/test-gh-react/static/js/runtime-main.1a038992.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kallavera/test-gh-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
